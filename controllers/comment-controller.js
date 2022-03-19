fab48aac6307a7e22f138a6425e52509f89e420f
jester <jaimebuddy98@gmail.com>
2022-03-19 12:08:16 -0500

    finsihed controller and added routes

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,13 +1,52 @@
+const { param } = require('express/lib/request');
+const res = require('express/lib/response');
 const { Pizza, Comment} = require('../models')
 
 const commentController = {
-    addComment() {
-
+    addComment({params, body}, res) {
+        console.log(body); 
+        Comment.create(body)
+            .then(({_id}) => {
+                console.log(_id)
+                return Pizza.findOneAndUpdate(
+                    {_id: params.pizzaId},
+                    {$push: {comments: _id}},
+                    {new: true}
+                );
+            })
+            .then(dbPizzaData => {
+                if(!dbPizzaData) {
+                    res.stats(404).json({message:"No pizza found with this id"});
+                    return;
+                }
+                res.json(dbPizzaData)
+            })
+            .catch(err => res.json(err));
     }, 
 
-    removeComment() {
 
+
+    removeComment() {
+        Comment.findOneAndDelete({_id: params.commentId})
+        .then(deletedComment => {
+            if(!deletedComment) {
+                return res.status(404).json({message: 'No comment with this id'});
+            }
+            return Pizza.findOneAndUpdate (
+                {_id: params.pizzaId},
+                { $pull:{comments: parmas.commentId}},
+                {new: true} 
+            );
+        })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
     }
 }; 
 
-module.exports = commentControllerS
+module.exports = commentController
